212abc10e99e3c453a87abefc27c5b7d7595e28f
Shane T. Fleming <t-shflem@microsoft.com>
2021-01-07 21:47:05 +0000

    Adding some example ESP32 code

--- ESP32/analog_in/analog_in.ino
+++ ESP32/analog_in/analog_in.ino
@@ -1,9 +1,14 @@
+const int asig_in = 34;
+int asig_val = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  asig_val = analogRead(asig_in);
+  Serial.println(asig_val);
 }
